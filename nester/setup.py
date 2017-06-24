bfe4b20862ff5d49091688dca1432073f67f645b
ubuntu <ubuntu@localhost>
2017-06-24 17:02:19 -0400

    Test

--- nester/setup.py
+++ nester/setup.py
@@ -4,8 +4,8 @@ setup(
     name='nester',
     version='1.0.0',
     py_modules=['nester'],
-    author='hfpython',
-    author_email='hfpython@headfirstlabs.com',
-    url='http://www.headfirstlabs.com',
+    author='leroneb',
+    author_email='leroneb23@gmail.com',
+    url='http://www.giftedbrain.com',
     description='A simple printer of nested lists',
 )
--- shareablecode_chp2.py
+++ shareablecode_chp2.py
@@ -8,4 +8,5 @@ cast = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
         ["Graham Chapman",
          ["Michael Palin", "John Cleese", "Eric Idle"]]]
 
-nester.print_lol(cast)
+
+nester.print_nester(cast)
