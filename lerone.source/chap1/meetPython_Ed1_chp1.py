0108f10d7d71e258fa7d63755d43f3d8c696b4a7
lerone bleasdille <lerone.bleasdille@gmail.com>
2017-06-25 20:52:47 -0400

    Working on chap 1 2nd edition.

--- lerone.source/chap1/meetPython_Ed1_chp1.py
+++ lerone.source/chap1/meetPython_Ed1_chp1.py
@@ -1,39 +1,40 @@
-#MEET PYTHON CHAP 1
+
+# MEET PYTHON CHAP 1
 #####################################
-print("hello" )
+print("hello")
 if 43 > 42:
     print("Don't panic!")
     print()
 
 
-#List of movies, or array of movies
+# List of movies, or array of movies
 movies = ["The Holy Grail",
           "The life of Brian",
           "The Meaning of Life"]
 
-#easier to print than Java
+# easier to print than Java
 print(movies)
 
-#lists are like arrays with the bottom/first item being item #0
+# lists are like arrays with the bottom/first item being item #0
 print(movies[0])
 print(len(movies))
 
-#append movie
+# append movie
 movies.append("Horse Raider")
 print(movies)
 
-movies.pop() #Pop last item off the list
+movies.pop()  # Pop last item off the list
 print(movies)
 
 movies.extend(["Tomb Raider", "Warrior Cry"])
 print(movies)
 
-#find and remove item
+# find and remove item
 movies.remove("Tomb Raider")
 movies.insert(0, "Tomb Fighter")
 print(movies)
 
-#Inserting date of the films
+# Inserting date of the films
 movies.insert(1, 2017)
 movies.insert(3, 1975)
 movies.insert(5, 1979)
@@ -41,7 +42,7 @@ movies.insert(7, 1983)
 movies.insert(9, 1999)
 print(movies)
 
-#Iterating over a list
+# Iterating over a list
 print()
 for eachMovie in movies:
     print(eachMovie)
@@ -55,7 +56,7 @@ print()
 print(movies[4][1][0])
 print(movies)
 
-#The for loop prints each item of the outer list once - it sees the inner list as just another list item
+# The for loop prints each item of the outer list once - it sees the inner list as just another list item
 for eachMovie in movies:
     print(eachMovie)
 
