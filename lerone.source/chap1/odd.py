c7fe2d9472c99f978aa4beda9fced62547d99e87
lerone bleasdille <lerone.bleasdille@gmail.com>
2017-06-26 23:50:31 -0400

    completing chap 1

--- lerone.source/chap1/odd.py
+++ lerone.source/chap1/odd.py
@@ -1,12 +1,20 @@
 from datetime import datetime
+from time import sleep
+import random
 
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
-right_this_minute = datetime.today().minute
+# odd2 using loops
+for i in range(4):  
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds: #This : introduces the suite of code.
+        print("This minute is odd")
+        print("current time is:", datetime.today())
+    else:
+        print("Not an odd minute")
+        print("current time is:", datetime.today())
+    sleep(random.randint(1,59))
 
-if right_this_minute in odds:
-    print("This minute is odd")
-else:
-    print("Not an odd minute")
+
     
-print("current time is:", datetime.today())
+
